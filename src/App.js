3803cff0096b7867798c0391de20629fa9292d35
finnluii <22249550+finnluii@users.noreply.github.com>
2019-08-20 01:02:39 -0400

    commit rough outline of app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class InfoBoard extends React.Component {
+  render() {
+    return(
+      <div>
+        <ol>HIGH</ol>
+        <ol>LOW</ol>
+        <ol>P.O.P.</ol>
+      </div>
+      );
+  }
+}
+
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> It's the weather. </h1>
+      <div className="weatherBlock">
+        <div className="weatherInfo">
+            <InfoBoard className="weatherInfo"/>
+        </div>
+        <div className="weatherInfo">
+            <InfoBoard/>
+        </div>
+        <div className="weatherInfo">
+            <InfoBoard/>
+        </div>
+        <div className="weatherInfo">
+            <InfoBoard/>
+        </div>
+        <div className="weatherInfo">
+            <InfoBoard/>
+        </div>
+        <div className="weatherInfo">
+            <InfoBoard/>
+        </div>
+        <div className="weatherInfo">
+            <InfoBoard/>
+        </div>
+      </div>
     </div>
-  );
+    );
+}
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;

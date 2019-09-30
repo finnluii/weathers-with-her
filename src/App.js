88449fdaa90e4bf662e00bedfa36142b66e2ac40
finnluii <22249550+finnluii@users.noreply.github.com>
2019-09-29 23:39:04 -0400

    Working calls to weather API given city + country code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 class Location extends React.Component {
 
 }
@@ -24,66 +25,128 @@ class InfoBoard extends React.Component {
   }
 }
 
+class Weather extends React.Component {
+  render() {
+    return (
+      <div className="weatherBlock">
+      {
+        // <div className="weatherInfo">
+        //     <InfoBoard/>
+        // </div>
+        // <div className="weatherInfo">
+        //     <InfoBoard/>
+        // </div>
+        // <div className="weatherInfo">
+        //     <InfoBoard/>
+        // </div>
+        // <div className="weatherInfo">
+        //     <InfoBoard/>
+        // </div>
+        // <div className="weatherInfo">
+        //     <InfoBoard/>
+        // </div>
+        // <div className="weatherInfo">
+        //     <InfoBoard/>
+        // </div>
+        // <div className="weatherInfo">
+        //     <InfoBoard/>
+        // </div>
+      }
+      </div>
+      );
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      location: null,
-    };
+    // this.state = {
+    //   city: '',
+    //   // country: '',
+    // };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
 
   }
 
   handleChange(event) {
-    this.setState({location: event.target.value});
-
+    // console.log("hello we're in handleChange()")
+    console.log("I'm here.");
+    console.log("hello" + event.target.value);
+    // this.setState({city: event.target.value});
 
   }
 
-  handleSubmit(event) {
-    alert('The city ' + this.state.location + ' has been submitted!');
+
+  async getWeather(event) {
+    // Don't refresh page
     event.preventDefault();
+
+    const city = event.target.elements.city.value;
+    const country = event.target.elements.country.value;
+    var found = false;
+    // Get cityID from city name
+    const data = require('./city.list.json');
+    
+    for (var i=0; i<data.length; i++) {
+      if (data[i].name.toLowerCase() == city.toLowerCase() && 
+        data[i].country.toLowerCase() == country.toLowerCase()){
+        found = true;
+        var cityId = data[i].id;
+        console.log(cityId);
+
+        // alert('The city ' + this.state.city + ' has been submitted!');
+      }
+    }
+
+    // TODO: update status "Are you sure the input is right?"
+    // if (found == false) {
+
+    // }
+
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/forecast?id='
+          + cityId
+          +'&appid=d801e2c4e7af34945bff26d22936710b');
+    const response = await api_call.json();
+    console.log(response);
+    
+
+
+    
+    
   }
+
+  // handleSubmit(event) {
+  //   alert('The city ' + this.state.city + ' has been submitted!');
+  //   event.preventDefault();
+  // }
+
+  
 
   render() {
+    /* Note: do this instead of calling this.getWeather() (will get Type Error),
+      Difference is: this.getWeather() = calling the function immediately.
+      this.getWeather = passing the reference of the function to call on submit.
+      */
   return (
     <div>
-      <h1> It's the weather. </h1>
-      <form onSubmit={this.handleSubmit}>
+      <h1> Well, if it isn't the weather. </h1>
+
+      <form onSubmit={this.getWeather}>
         <label>
-          City:
-          <input type="text" name="title" value={this.state.location} onChange={this.handleChange} />
+        
+          <input type="text" name="city" placeholder="City"
+          />
+
+          <input type="text" name="country" placeholder="Country" 
+          />
+        
         </label>
         <input type="submit" value="Find the weather for my city!"/>
       </form>
       
-
-
-      <div className="weatherBlock">
-        <div className="weatherInfo">
-            <InfoBoard className="weatherInfo"/>
-        </div>
-        <div className="weatherInfo">
-            <InfoBoard/>
-        </div>
-        <div className="weatherInfo">
-            <InfoBoard/>
-        </div>
-        <div className="weatherInfo">
-            <InfoBoard/>
-        </div>
-        <div className="weatherInfo">
-            <InfoBoard/>
-        </div>
-        <div className="weatherInfo">
-            <InfoBoard/>
-        </div>
-        <div className="weatherInfo">
-            <InfoBoard/>
-        </div>
-      </div>
+      <Weather/> 
     </div>
     );
 }

de97d6c7916fe8b966370350bf52076348c34196
finnluii <22249550+finnluii@users.noreply.github.com>
2019-10-13 17:42:33 -0400

    Weather working + display icon showing. next: checklist

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 // import { isoCountries } from './countryCodes.js'
 
-class Location extends React.Component {
+// class Location extends React.Component {
 
-}
+// }
 
 class InfoBoard extends React.Component {
   constructor(props) {
@@ -28,42 +28,58 @@ class InfoBoard extends React.Component {
 
 class Weather extends React.Component {
   render() {
+
+
+
     return (
       <div className="weatherBlock">
         <div>
-          <p>{this.props.city}</p>
-          <p>{this.props.country}</p>
-          <p>{this.props.temperature}</p>
-          <p>{this.props.humidity}</p>
-          <p>{this.props.low}</p>
-          <p>{this.props.high}</p>
+        {
+          // Check if there is valid input before displaying weather info
+        } {
+
+          this.props.code &&
+          <img src={"http://openweathermap.org/img/wn/"+ this.props.code + "@2x.png"} alt="weather icon"/>
+        } {
+          this.props.city && 
+          <p> <span> City: </span> {this.props.city}</p>
+        } {
+          this.props.country &&
+          <p> Country: {this.props.country}</p>
+        } {
+          this.props.temperature &&
+          <p>Temperature: {this.props.temperature}°</p>
+        } {
+          this.props.humidity && 
+          <p> Humidity: {this.props.humidity}</p>
+        } {
+          this.props.low &&
+          <p>Low: {this.props.low}°</p>
+        } {
+          this.props.high &&
+          <p>High: {this.props.high}°</p>
+        } {
+          this.props.description &&
+          <div>
+            <p>Description: {this.props.description}</p>
+          </div>
+        } {
+          this.props.error &&
+          <p>{this.props.error}</p>
+        } 
         </div>
-      {
-        // <div className="weatherInfo">
-        //     <InfoBoard/>
-        // </div>
-        // <div className="weatherInfo">
-        //     <InfoBoard/>
-        // </div>
-        // <div className="weatherInfo">
-        //     <InfoBoard/>
-        // </div>
-        // <div className="weatherInfo">
-        //     <InfoBoard/>
-        // </div>
-        // <div className="weatherInfo">
-        //     <InfoBoard/>
-        // </div>
-        // <div className="weatherInfo">
-        //     <InfoBoard/>
-        // </div>
-        // <div className="weatherInfo">
-        //     <InfoBoard/>
-        // </div>
-      }
       </div>
       );
   }
+}
+
+class Checklist extends React.Component {
+  render() {
+    // Create checklist
+    return null;
+  }
+
+  createList() {}
 }
 
 class App extends React.Component {
@@ -76,6 +92,11 @@ class App extends React.Component {
       humidity: undefined,
       low: undefined,
       high: undefined,
+      description: undefined,
+      code: undefined,
+      id: undefined,
+      error: undefined,
+      isMetric: true,
     };
 
     // this.handleChange = this.handleChange.bind(this);
@@ -91,8 +112,10 @@ class App extends React.Component {
 
   }
 
-  // // Below doesn't work because you have to bind the function
+  // // Use arrow function to bind the function
   // async getWeather(event) {
+
+  // Bind 'this' to its context, otherwise this (during setState) is null.
   getWeather = async (event) => {
 
     // Don't refresh page
@@ -104,55 +127,86 @@ class App extends React.Component {
     // Get cityID from city name
     const data = require('./city.list.json');
     const countryData = require('./countryCodes.json');
-    var isMetric = true;
-
-    // Find country code from country name.
-    for (var j=0;j<countryData.length; j++) {
-      if (countryData[j].name.toLowerCase() === country.toLowerCase()) {
-        var countryCode = countryData[j].code;
-        break;
+    if (city && country) {
+      // Find country code from country name.
+      for (var j=0;j<countryData.length; j++) {
+        if (countryData[j].name.toLowerCase() === country.toLowerCase()) {
+          var countryCode = countryData[j].code;
+          break;
+        }
       }
-    }
-    
 
-    for (var i=0; i<data.length; i++) {
-      if (data[i].name.toLowerCase() === city.toLowerCase() && 
-        data[i].country.toLowerCase() === countryCode.toLowerCase()){
-        found = true;
-        var cityId = data[i].id;
-        break;
+      for (var i=0; i<data.length; i++) {
+        if (data[i].name.toLowerCase() === city.toLowerCase() && 
+          data[i].country.toLowerCase() === countryCode.toLowerCase()){
+          found = true;
+          var cityId = data[i].id;
+          break;
+        }
       }
-    }
+      if (cityId) {
+        // TODO: METRIC OR IMPERIAL???
+      
+        var call_str = 'http://api.openweathermap.org/data/2.5/weather?id='
+              + cityId
+              +'&appid=d801e2c4e7af34945bff26d22936710b';
 
-    // TODO: METRIC OR IMPERIAL???
+        if (this.state.isMetric) {
+          call_str = call_str + "&units=metric";
+        } else {
+          call_str = call_str + "&units=imperial";
+        }
+        const api_call = await fetch(call_str);
+        
+        const response = await api_call.json();
+        console.log(response);
+        console.log(response.weather[0].icon);
 
-    // TODO: update status "Are you sure the input is right?" error handling
-    // if (found == false) {
-
-    // }
-
-    var call_str = 'http://api.openweathermap.org/data/2.5/weather?id='
-          + cityId
-          +'&appid=d801e2c4e7af34945bff26d22936710b';
-    if (isMetric) {
-      call_str = call_str + "&units=metric";
+        // TODO: update status "Are you sure the input is right?" error handling
+        // Update state
+        this.setState({
+          city: response.name,
+          country: response.sys.country,
+          temperature: response.main.temp,
+          humidity: response.main.humidity,
+          low: response.main.temp_min,
+          high: response.main.temp_max,
+          description: response.weather[0].description,
+          code: response.weather[0].icon,
+          id: response.weather[0].id,
+          error: undefined,
+        });
+        
+      } else {
+        this.setState({
+          city: undefined,
+          country: undefined,
+          temperature: undefined,
+          humidity: undefined,
+          low: undefined,
+          high: undefined,
+          description: undefined,
+          code: undefined,
+          id: undefined,
+          error: "Error. Please make sure your spelling is correct/Enter the full name " +
+          "of your city & country!",
+        });
+      }
     } else {
-      call_str = call_str + "&units=imperial";
+      this.setState({
+        city: undefined,
+        country: undefined,
+        temperature: undefined,
+        humidity: undefined,
+        low: undefined,
+        high: undefined,
+        description: undefined,
+        code: undefined,
+        id: undefined,
+        error: "Error. Please specify the city and country!",
+      });
     }
-    const api_call = await fetch(call_str);
     
-    const response = await api_call.json();
-    // console.log(response.name);
-
-    // Update state
-    this.setState({
-      city: response.name,
-      country: response.sys.country,
-      temperature: response.main.temp,
-      humidity: response.main.humidity,
-      low: response.main.temp_min,
-      high: response.main.temp_max,
-    });
     
 
 
@@ -169,14 +223,15 @@ class App extends React.Component {
   
 
   render() {
-    /* Note: do this instead of calling this.getWeather() (will get Type Error),
-      Difference is: this.getWeather() = calling the function immediately.
-      this.getWeather = passing the reference of the function to call on submit.
-      */
   return (
     <div>
-      <h1> Well, if it isn't the weather. </h1>
-
+      <h1> Hey, Mother Nature. What should I wear today? </h1>
+      {
+      //    Note: do this instead of calling this.getWeather() (will get Type Error),
+      // Difference is: this.getWeather() = calling the function immediately.
+      // this.getWeather = passing the reference of the function to call on submit.
+      
+      }
       <form onSubmit={this.getWeather}>
         <label>
         
@@ -197,30 +252,24 @@ class App extends React.Component {
         humidity={this.state.humidity}
         low={this.state.low}
         high={this.state.high}
-        /> 
+        description={this.state.description}
+        code={this.state.code}
+        error={this.state.error}
+      /> 
+
+      <Checklist
+        temperature={this.state.temperature}
+        humidity={this.state.humidity}
+        low={this.state.low}
+        high={this.state.high}
+        id={this.state.id}
+      />
     </div>
 
 
     );
 }
 
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;

586a9a9c6987665ce7038365ddf492104b9783c3
finnluii <22249550+finnluii@users.noreply.github.com>
2019-10-15 12:34:34 -0400

    we did it fam, now we have some css and minor adjustments to do but its all workin yaaaay

--- src/App.js
+++ src/App.js
@@ -88,44 +88,50 @@ class Checklist extends React.Component {
 
   render() {
     // Always wear sunscreen! Or else the UV rays will give you wrinkles >:(
-    var itemsToBring = ["SUNSCREEN!"];
+    if (this.props.id) { 
+      var itemsToBring = ["SUNSCREEN!"]; 
+    
 
+      // If it's raining, bring an umbrella!
+      const id = this.props.id;
+      var rain_pattern = /^[2,3,5][0-9]{2}$/;
 
-    // If it's raining, bring an umbrella!
-    const id = this.props.id;
-    var rain_pattern = /^[2,3,5][0-9]{2}$/;
+      if (rain_pattern.test(id)) { 
+        itemsToBring.push("Umbrella"); 
+      }
 
-    if (rain_pattern.test(id)) { 
-      itemsToBring.push("Umbrella"); 
+      // Add clothes depending on temperature ranges
+      const temp = this.props.temperature;
+      if (temp >= 25) {
+        itemsToBring.push("t-shirt", "shorts/summer, short skirt", "sandals");
+      } else if (temp >= 18 && temp < 25) {
+        itemsToBring.push("short/long-sleeve t-shirt", "pants/jeans/short-midi skirt");
+      } else if (temp >= 10 && temp < 18) {
+        itemsToBring.push("sweater/fall jacket", "pants/jeans/midi skirt");
+      } else if (temp >= 4 && temp < 10) {
+        itemsToBring.push("fall jacket", "sweater", "pants/jeans/warm, long skirt", "booties",
+          "winter hat");
+      } else if (temp >= -10 && temp < 4) {
+        itemsToBring.push("winter jacket", "long-sleeve shirt", "pants/jeans/warm, long skirt", 
+          "winter boots", "winter hat");
+      } else if (temp < -10) {
+        itemsToBring.push("winter jacket", "sweater", "pants/jeans/warm, long skirt", "winter boots",
+          "thick socks", "winter hat", "gloves", "scarf");
+      }
+
+      console.log(itemsToBring);
+      const checklist = itemsToBring.map((x) => {
+        return <div><input type="checkbox" key={x}/>{x}</div>
+      });
+
+      // return checklist
+      return (
+        <div>
+          {checklist}
+        </div>
+        );
     }
-
-    // Add clothes depending on temperature ranges
-    const temp = this.props.temperature;
-    if (temp >= 25) {
-      itemsToBring.push("t-shirt", "shorts/summer, short skirt", "sandals");
-    } else if (temp >= 18 && temp < 25) {
-      itemsToBring.push("short/long-sleeve t-shirt", "pants/jeans/short-midi skirt");
-    } else if (temp >= 10 && temp < 18) {
-      itemsToBring.push("sweater/fall jacket", "pants/jeans/midi skirt");
-    } else if (temp >= 4 && temp < 10) {
-      itemsToBring.push("fall jacket", "sweater", "pants/jeans/warm, long skirt", "booties",
-        "winter hat");
-    } else if (temp >= -10 && temp < 4) {
-      itemsToBring.push("winter jacket", "long-sleeve shirt", "pants/jeans/warm, long skirt", 
-        "winter boots", "winter hat");
-    } else if (temp < -10) {
-      itemsToBring.push("winter jacket", "sweater", "pants/jeans/warm, long skirt", "winter boots",
-        "thick socks", "winter hat", "gloves", "scarf");
-    }
-
-    console.log(itemsToBring);
-
-    // return checklist
-    return (
-      <div>
-        
-      </div>
-      );
+    return null;
   }
 
   
